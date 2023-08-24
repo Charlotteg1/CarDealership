bf4559233aa39ab49d01ea40ef9e54fd26f20f5c
charlotteg1 <charlotte.gould3@gmail.com>
2023-08-24 09:15:28 +0100

    added to dealership

--- Dealership.js
+++ Dealership.js
@@ -1,7 +1,9 @@
-const Dealership = function(name , maxCars, numberInStock){
+const Car = require('./Car');
+
+const Dealership = function(name , maxCars, carsInStock){
     this.name = name;
-    this.maxCars = maxcars;
-    this.numberInStock = numberInStock;
+    this.maxCars = maxCars;
+    this.carsInStock = [];
 }
 
 
@@ -17,10 +19,27 @@ Dealership.prototype.setMaxCars = function(maxCars) {
 
 Dealership.prototype.getMaxCars = function() {return this.maxCars;}
 
-Dealership.prototype.setNumberInStock = function(numberInStock) {
-    this.numberInStock = numberInStock;
+Dealership.prototype.setCarsInStock = function(carsInStock) {
+    this.carsInStock = carsInStock;
 }
 
-Dealership.prototype.getNumberInStock = function() {return this.numberInStock;}
+Dealership.prototype.getCarsInStock = function() {return this.carsInStock;}
 
+Dealership.prototype.getNumberOfCarsInStock = function() {return this.carsInStock.length;}
+
+Dealership.prototype.addCarToStock = function(Car) {this.carsInStock.push(Car);}
+
+Dealership.prototype.getEachManufacturer = function() {
+    const manufacturers = new Array()
+     this.carsInStock.forEach(car => {manufacturers.push(car.getManufacturer());})
+     return manufacturers;
+}
+
+Dealership.prototype.getCarsByManufacturer = function(manufacturer) {
+    return this.carsInStock.filter(car => car.getManufacturer() === manufacturer);
+}
+
+Dealership.prototype.getTotalValue = function(manufacturer) {
+    return this.carsInStock.reduce((total, car) => total + car.getPrice(), 0);
+};
 module.exports = Dealership;
---
+++ Dealership.test.js
@@ -0,0 +1,41 @@
+const Dealership = require('./Dealership');
+
+const dealership = new Dealership("Car World", 10, []);
+
+const Car = require('./Car');
+
+const car1 = new Car("Audi", 200, "Petrol");
+const car2 = new Car("Volvo", 100, "Petrol");
+const car3 = new Car("BMW", 400, "Electric");
+
+dealership.addCarToStock(car1);
+dealership.addCarToStock(car2);
+dealership.addCarToStock(car3);
+
+describe('Dealership tests', () => {
+
+    // testing add to stock and getNumberOfCarsInStock
+    test('can get number of cars in stock ', () => {
+        const expected = 3;
+        const actual = dealership.getNumberOfCarsInStock();
+        expect(actual).toBe(expected);
+    });
+
+    test('can return manufacturers', () => {
+        const expected = ["Audi", "Volvo", "BMW"];
+        const actual = dealership.getEachManufacturer();
+        expect(actual).toBe(expected);
+    });
+
+    test('can filter by manufacturer', () => {
+        const expected = [new Car("Audi", "£200", "Petrol")];
+        const actual = dealership.getEachManufacturer();
+        expect(actual).toBe(expected);
+    });
+
+    test('can total value', () => {
+        const expected = 700 ;
+        const actual = dealership.getTotalValue();
+        expect(actual).toBe(expected);
+    });
+})
